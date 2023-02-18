8c653fa6e8a3e6b8550b47ce97936bd183f0e5cf
maeldurrer <125691917+maeldurrer@users.noreply.github.com>
2023-02-18 11:49:15 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@ radio.onReceivedString(function (receivedString) {
             . . # . .
             . . # . .
             `)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
     } else if (receivedString == "zurück") {
         basic.showLeds(`
             . . # . .
@@ -15,6 +16,27 @@ radio.onReceivedString(function (receivedString) {
             . # # # .
             . . # . .
             `)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+    } else if (receivedString == "links") {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        maqueen.motorStop(maqueen.Motors.M1)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+    } else if (receivedString == "rechts") {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        maqueen.motorStop(maqueen.Motors.M2)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
     } else {
         basic.showLeds(`
             . . . . .
@@ -23,6 +45,7 @@ radio.onReceivedString(function (receivedString) {
             . . . . .
             . . . . .
             `)
+        maqueen.motorStop(maqueen.Motors.All)
     }
 })
 radio.setGroup(1)
